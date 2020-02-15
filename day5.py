49b419adc0ed50b3cf1cf4f5c9b7d3d67ae63aea
kal <fkalkidan30@gmail.com>
2020-02-15 16:03:34 +0300

    Solve Day5's problem

---
+++ day5.py
@@ -0,0 +1,19 @@
+"""
+This problem was asked by Dropbox.
+
+What does the below code snippet print out?
+How can we fix the anonymous functions to 
+behave as we'd expect?
+
+"""
+functions = []
+for i in range(10):
+    functions.append(lambda i=i : i)
+
+for f in functions:
+    print(f())
+
+"""
+Reference 
+https://stackoverflow.com/questions/58280201/lambda-function-call
+"""
